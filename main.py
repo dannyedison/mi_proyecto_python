ecd207ad880d011916d414302f087a6c2988e244
dannyedison75 <proyectosdanny75@gmail.com>
2024-08-23 20:32:14 -0500

    Se agrego suma

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ if __name__ == "__main__":
     nombre = input("Ingrese su nombre: ")
     saludo = saludar(nombre)
     print(saludo)
+
+numero_uno = 5
+numero_dos = 25
+suma = numero_uno + numero_dos
